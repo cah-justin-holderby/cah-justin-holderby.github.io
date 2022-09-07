1e2b9c29c757fa1607b00ab3c933b5b9b848785b
Justin Holderby <justin.holderby@cardinalhealth.com>
2022-09-07 13:28:24 -0400

    Parallax images and new section.

--- js/website-javascript.js
+++ js/website-javascript.js
@@ -1,30 +1,26 @@
-var onScrollHandler = () => {
-    var newImageUrl = yourImageElement.src;
-    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-    if (scrollTop > 100) {
-        newImageUrl = "spaceFrame1.png"
-    }
-    if (scrollTop > 200) {
-        newImageUrl = "spaceFrame2.png"
-    }
-    if (scrollTop > 300) {
-        newImageUrl = "spaceFrame3.png"
-    }
-    if (scrollTop > 400) {
-        newImageUrl = "spaceFrame4.png"
-    }
-    if (scrollTop > 500) {
-        newImageUrl = "spaceFrame5.png"
-    }
-    if (scrollTop > 600) {
-        newImageUrl = "spaceFrame6.png"
-    }
-    if (scrollTop > 700) {
-        newImageUrl = "spaceFrame7.png"
-    }
-    if (scrollTop > 800) {
-        newImageUrl = "spaceFrame8.png"
-    }
-    yourImageElement.src = newImageUrl;
-};
-object.addEventListener ("scroll", onScrollHandler);
+const scrollImages = {
+    1: '/images/spaceFrame1.png',
+    2 : '/images/spaceFrame2.png',
+    3 : '/images/spaceFrame3.png',
+    4 : '/images/spaceFrame4.png',
+    5 : '/images/spaceFrame5.png',
+    6 : '/images/spaceFrame6.png',
+    7 : '/images/spaceFrame7.png',
+    8 : '/images/spaceFrame8.png',
+}
+
+// Global variable to control the scrolling behavior
+const step = 30; // For each 30px, change an image
+function trackScrollPosition() {
+    const y = window.scrollY;
+    const label = Math.min(Math.floor(y/step) + 1, 8);
+    const imageToUse = scrollImages[label];
+    // Change the background image
+    document.getElementById("spaceimage").src=imageToUse;
+}
+
+$(document).ready(()=>{
+    $(window).scroll(()=>{
+        trackScrollPosition();
+    })
+})
