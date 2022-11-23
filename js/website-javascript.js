3be647a38c4c740f41d2c133b5f12ca5a6375d56
Justin Holderby <justin.holderby@cardinalhealth.com>
2022-11-23 10:31:00 -0500

    Trying a new menu from codepen

--- js/website-javascript.js
+++ js/website-javascript.js
@@ -1,26 +1,3 @@
-const scrollImages = {
-    1: '/images/spaceFrame1.png',
-    2 : '/images/spaceFrame2.png',
-    3 : '/images/spaceFrame3.png',
-    4 : '/images/spaceFrame4.png',
-    5 : '/images/spaceFrame5.png',
-    6 : '/images/spaceFrame6.png',
-    7 : '/images/spaceFrame7.png',
-    8 : '/images/spaceFrame8.png',
-}
-
-// Global variable to control the scrolling behavior
-const step = 30; // For each 30px, change an image
-function trackScrollPosition() {
-    const y = window.scrollY;
-    const label = Math.min(Math.floor(y/step) + 1, 8);
-    const imageToUse = scrollImages[label];
-    // Change the background image
-    document.getElementById("spaceimage").src=imageToUse;
-}
-
-$(document).ready(()=>{
-    $(window).scroll(()=>{
-        trackScrollPosition();
-    })
-})
+$(".menu").click(function(){
+    $(this).parent().toggleClass("close");
+});
